a6ad108947e306955908738a7323ac5f9b838106
Abhishek Amralkar <abhishekamralkar@gmail.com>
2022-11-04 22:03:49 +0530

    added a flag package and now hwt accepts 2 flags city and unit

--- main.go
+++ main.go
@@ -2,17 +2,18 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
+	"flag"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
+	"text/template"
 )
 
-const weatherTemplate = `Current weather for {{.Name}}:
-    Now:         {{.Main.Temp}} {{.Unit}}
-    High:        {{.Main.TempMax}} {{.Unit}}
-    Low:         {{.Main.TempMin}} {{.Unit}}
+const weatherTemplate = `Current weather in {{.Name}}:
+    Now:         {{.Main.FeelsLike}}
+    High:        {{.Main.Max}}
+    Low:         {{.Main.Min}}
 `
 
 type data struct {
@@ -29,7 +30,7 @@ type coord struct {
 
 type mainData struct {
 	Temp        float64 `json:"temp"`
-	FeelsLike   float64 `json:"feels_liks"`
+	FeelsLike   float64 `json:"feels_like"`
 	Min         float64 `json:"temp_min"`
 	Max         float64 `json:"temp_max"`
 	Pressure    float64 `json:"pressure"`
@@ -38,10 +39,14 @@ type mainData struct {
 	GroundLevel float64 `json:"grnd_leve"`
 }
 
-func getData(city string) float64 {
+func main() {
 	var apiKey = os.Getenv("OWM_API_KEY")
 
-	url := "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey
+	city := flag.String("city", "Pune", "Enter the city name.")
+	units := flag.String("units", "kelvin", "Supports Kelvin, Celcius(metric), Fahrenheit(standard)")
+
+	flag.Parse()
+	url := "https://api.openweathermap.org/data/2.5/weather?q=" + *city + "&appid=" + apiKey + "&units=" + *units
 
 	resp, err := http.Get(url)
 	if err != nil {
@@ -57,14 +62,23 @@ func getData(city string) float64 {
 	jsonErr := json.Unmarshal(body, &data_obj)
 	if jsonErr != nil {
 		log.Fatal(jsonErr)
+		return
 	}
 
-	lang := data_obj.Coord.Lat
+	//create a new template with some name
+	tmpl := template.New("weather")
 
-	return lang
-}
+	//parse some content and generate a template
+	tmpl, err = tmpl.Parse(weatherTemplate)
+	if err != nil {
+		log.Fatal("Parse: ", err)
+		return
+	}
 
-func main() {
-	fmt.Println(getData("pune"))
+	err1 := tmpl.Execute(os.Stdout, data_obj)
+	if err1 != nil {
+		log.Fatal("Execute: ", err1)
+		return
+	}
 
 }
